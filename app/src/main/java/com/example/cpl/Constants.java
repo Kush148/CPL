7d3275aeb891ce2cd8d75580912f6b33dc836276
Darshita <darshitamalaviya740@gmail.com>
2020-03-13 13:14:20 -0400

    add spinner and modification in pointtable class

--- app/src/main/java/com/example/cpl/Constants.java
+++ app/src/main/java/com/example/cpl/Constants.java
@@ -1,7 +1,7 @@
 package com.example.cpl;
 
 public class Constants {
-    public static String localHost = "172.26.221.16:8080";
+    public static String localHost = "192.168.2.30:8080";
     public static String projectPath = "WebServicesCPL/cpl/";
     public static String userId = "";
     public static String userType = "";
--- app/src/main/java/com/example/cpl/PointTable.java
+++ app/src/main/java/com/example/cpl/PointTable.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -23,7 +27,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import static java.sql.Types.NULL;
 
 
 /**
@@ -34,8 +41,13 @@ public class PointTable extends Fragment {
     List<PointsTableList> pointsTableLists = new ArrayList<>();
     RecyclerView rvPointsTableList;
     private ProgressBar progressBar;
-
-
+    public ArrayList<String> Seasonlist = new ArrayList<>();
+    private String seasonList = null;
+    Spinner spinner;
+    int seasonId;
+    private int val= 0;
+    public HashMap<String, Integer> HSeasonList = new HashMap<>();
+    public ArrayList<String> TeamNames = new ArrayList<String>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -44,10 +56,111 @@ public class PointTable extends Fragment {
 
         rvPointsTableList = view.findViewById(R.id.rvPointsTable);
         progressBar = view.findViewById(R.id.progressBar);
-        new MyTask().execute();
+        spinner=(Spinner)view.findViewById(R.id.spinner);
+        new MyTask1().execute();
+
 
         return view;
     }
+    private class MyTask1 extends AsyncTask<Void, Void, Void> {
+        String return_msg;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            URL url = null;
+            try {
+
+                url = new URL("http://" + Constants.localHost + "/" + Constants.projectPath + "main/viewSeason");
+
+                HttpURLConnection client = null;
+
+                client = (HttpURLConnection) url.openConnection();
+
+                client.setRequestMethod("GET");
+
+                int responseCode = client.getResponseCode();
+
+                System.out.println("\n Sending 'GET' request to URL : " + url);
+
+                System.out.println("Response Code : " + responseCode);
+
+                InputStreamReader myInput = new InputStreamReader(client.getInputStream());
+
+                BufferedReader in = new BufferedReader(myInput);
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                    System.out.println("while " + response);
+                }
+                in.close();
+
+                //print result
+                System.out.println(response.toString());
+
+                JSONObject mainObject = new JSONObject(response.toString());
+                return_msg = mainObject.getString("Status");
+
+                JSONArray teamManagerArray = mainObject.getJSONArray("Seasons");
+                JSONObject singlename;
+                for (int i = 0; i < teamManagerArray.length(); i++) {
+                    singlename = teamManagerArray.getJSONObject(i);
+                    String teamName = singlename.getString("Season Title");
+                    int teamId = singlename.getInt("SeasonId");
+                    System.out.println(teamName);
+                    HSeasonList.put(teamName,teamId);
+                    System.out.println("..........."+HSeasonList);
+                    for ( String key : HSeasonList.keySet() ) {
+                        TeamNames.add(key);
+                    }
+
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            ArrayAdapter SAdapter = new ArrayAdapter(getContext(), R.layout.single_spinnerdata,TeamNames);
+            SAdapter.setDropDownViewResource(R.layout.single_spinnerdata);
+            spinner.setAdapter(SAdapter);
+
+            spinner.setOnItemSelectedListener(new PointTable.MyOnItemSelectedListener());
+
+
+        }
+    }
+
+    private class MyOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String selectedItem = parent.getItemAtPosition(position).toString();
+
+            seasonList = selectedItem;
+            val = (int)HSeasonList.get(seasonList);
+            System.out.println(val);
+            if(val != NULL )
+            {
+                new MyTask().execute();}
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+
 
     private class MyTask extends AsyncTask<Void, Void, Void> {
         String return_msg;
@@ -63,7 +176,7 @@ public class PointTable extends Fragment {
             URL url = null;
             try {
 
-                url = new URL("http://192.168.2.32:8080/WebServicesCPL/cpl/main/viewPointsTable&1");
+                url = new URL("http://192.168.2.30:8080/WebServicesCPL/cpl/main/viewPointsTable&"+val);
 
                 HttpURLConnection client = null;
 
@@ -127,13 +240,21 @@ public class PointTable extends Fragment {
         @Override
         protected void onPostExecute(Void result) {
 
-             rvPointsTableList.setLayoutManager(new LinearLayoutManager(getActivity()));
-            PointsTableAdapter showTableAdapter = new PointsTableAdapter(pointsTableLists,getActivity());
-            rvPointsTableList.setAdapter(showTableAdapter);
-            progressBar.setVisibility(View.INVISIBLE);
+            if(return_msg.equals("null"))
+            {
+                progressBar.setVisibility(View.GONE);
+
+            }
+            else
+            {
+                rvPointsTableList.setLayoutManager(new LinearLayoutManager(getActivity()));
+                PointsTableAdapter showTableAdapter = new PointsTableAdapter(pointsTableLists,getActivity());
+                rvPointsTableList.setAdapter(showTableAdapter);
+                progressBar.setVisibility(View.INVISIBLE);}
 
             super.onPostExecute(result);
         }
     }
+
 
 }
